655fbbb288ca4bc2d3449b63e6285a8af02494e2
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2023-11-08 12:59:42 -0500

    SHOW&CREATE

--- controllers/idsController.js
+++ controllers/idsController.js
@@ -1,15 +1,32 @@
 
 const express = require("express");
 const ids = express.Router();
-const { getAllIds } = require("../queries/ids.js")
+const { getAllIds, getId, createId } = require("../queries/ids.js")
 // INDEX
 ids.get("/", async (req, res) => {
     const allIds = await getAllIds();
-    // if (allIds[0]) {
+    if (allIds[0]) {
         res.status(200).json(allIds);
-    // } else {
-    //     res.status(500).json({ error: "server error" });
-    // }
+    } else {
+        res.status(500).json({ error: "server error" });
+    }
+});
+
+// SHOW
+ids.get("/:key", async (req, res) => {
+    const { key } = req.params;
+    const id = await getId(key);
+    if (id) {
+        res.json(id);
+    } else {
+        res.status(404).json({ error: "not found" });
+    }
+});
+
+//CREATE
+ids.post("/", async (req, res) => {
+    const id = await createId(req.body);
+    res.json(id);
 });
 
 module.exports = ids;
--- queries/ids.js
+++ queries/ids.js
@@ -9,4 +9,29 @@ const getAllIds = async () => {
     }
 };
 
-module.exports = { getAllIds };
+const getId = async () => {
+    try {
+        const oneId = await db.one("SELECT * FROM ids WHERE key=$1", key);
+        return oneId;
+    } catch (error) {
+        return error;
+    }
+};
+
+const createId = async (id) => {
+    try {
+        const newId = await db.one(
+            "INSERT INTO ids (alias, lastname, dob, adult) VALUES($1, $2) RETURNING *", [id.alias, id.lastname, id.dob, id.adult]
+        );
+        return newId
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+module.exports = {
+    getAllIds,
+    getId,
+    createId,
+};
